2dc9d0905f7a8e8619f51aedb44f1a8048625450
Philip <philip.bernstrom@elev.ga.ntig.se>
2022-05-18 14:49:44 +0200

    Update Pokemon.cs

--- Web_API/Properties/Pokemon.cs
+++ Web_API/Properties/Pokemon.cs
@@ -1,9 +1,11 @@
 using System;
-
+using System.Collections.Generic;
 
 public class Pokemon
 {
     public string name { get; set; }
-    public int Hp { get; set; }
-    public string Type { get; set; }
+
+    public int id { get; set; }
+    public bool is_default { get; set; }
+    public List<ItemWrapper> held_items { get; set; }
 }
